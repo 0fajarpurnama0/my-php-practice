b8999c9f3411555036e5e486005db4607286bcac
0fajarpurnama0 <fajar.purnama@yahoo.com>
2017-10-24 22:24:11 +0900

    new file:   4-interface/104fe2f24b3c3d02b21e1fb3d61eeb6a 	new file:   cssout/cssout 	new file:   php-ajax-polls/poll_result.txt 	new file:   php-ajax-polls/poll_vote.php 	new file:   php-ajax-polls/template.html  Changes not staged for commit: 	modified:   4-interface/104fe2f24b3c3d02b21e1fb3d61eeb6a (modified content)
     Changes to be committed:
    	new file:   4-interface/104fe2f24b3c3d02b21e1fb3d61eeb6a
    	new file:   cssout/cssout
    	new file:   php-ajax-polls/poll_result.txt
    	new file:   php-ajax-polls/poll_vote.php
    	new file:   php-ajax-polls/template.html
     Changes not staged for commit:
    	modified:   4-interface/104fe2f24b3c3d02b21e1fb3d61eeb6a (modified content)

---
+++ php-ajax-polls/poll_vote.php
@@ -0,0 +1,47 @@
+<?php
+$vote = $_REQUEST['vote'];
+
+//get content of textfile
+$filename = "poll_result.txt";
+$content = file($filename);
+
+//put content in array
+$array = explode("||", $content[0]);
+$zero = $array[0];
+$one = $array[1];
+
+if ($vote == 0) {
+  $zero = $zero + 1;
+}
+if ($vote == 1) {
+  $one = $one + 1;
+}
+
+//insert votes to txt file
+$insertvote = $zero."||".$one;
+$fp = fopen($filename,"w");
+fputs($fp,$insertvote);
+fclose($fp);
+?>
+
+<h2>Result:</h2>
+<table>
+<tr>
+<td>Much Worse:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($yes/($no+$yes),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>No:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($no/($no+$yes),2)); ?>%
+</td>
+</tr>
+</table>
